a346655a9a006bf3f45f93e1046f19b65fcb6635
the.poche <a.pochettino@hotmail.com>
2021-04-05 01:17:49 -0300

    almost ready

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const menuBar = document.querySelector('.menu-bar02');
+const navBar = document.querySelector('.nav-bar02');
+
+menuBar.addEventListener('click', () => {
+  navBar.classList.toggle('toggle');
+});
+
+
+
+ $(window).scroll(function(){
+ $('nav').toggleClass('scrolled', $(this).scrollTop() > 1080);
+ });
